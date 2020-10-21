11428ebe2bbcabaf96ab3d4c0619f4245f7b6d92
WalyssonPaiva <walyssonpaiva@outlook.com>
2020-10-21 19:48:54 -0300

    Imported and created 'cor' attribute

--- src/models/no.ts
+++ src/models/no.ts
@@ -1,11 +1,15 @@
+import { Cor } from '../Enums/CorEnum';
+
 export default class No {
     esquerda: No;
     direita: No;
     chave: number;
+    cor: Cor;
 
     constructor(chave: number) {
         this.chave = chave;
         this.esquerda = null;
         this.direita = null;
+        this.cor = Cor.Vermelho;
     }
 }
